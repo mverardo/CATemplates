9f862517d7288136dab689dd2a3475b0073a9f70
mverardo <mauricio.verardo@gmail.com>
2015-07-26 18:17:33 -0300

    Adding the RawExpansion function, which performs the expansion and leaves the result as is (no filtering or mod applied)

---
+++ TemplateOperations/Expansion/Kernel/init.m
@@ -0,0 +1,6 @@
+(* Mathematica source file  *)
+(* Created by IntelliJ IDEA *)
+(* :Author: mverardo *)
+(* :Date: 26/07/15 *)
+
+Get["CATemplates`TemplateOperations`Expansion`RawExpansion`"];
---
+++ TemplateOperations/Expansion/RawExpansion.m
@@ -0,0 +1,37 @@
+(* Mathematica source file  *)
+(* Created by IntelliJ IDEA *)
+(* :Author: mverardo *)
+(* :Date: 26/07/15 *)
+
+BeginPackage["CATemplates`TemplateOperations`Expansion`RawExpansion`", "CATemplates`Basic`"];
+
+RawExpansion::usage =
+    "RawExpansion[T_Template] := performs a full expansion on template T, and leaves the result as it is.
+RawExpansion[T_Template, i_Integer] := performs the ith expansion on template T, and leaves the result as it is.";
+
+Begin["`Private`"];
+
+RawExpansion[template_Association, i_Integer] :=
+    Module[
+      {
+        k = template[["k"]],
+        r = template[["r"]],
+        variables = RuleTemplateVars[template],
+        substitution,
+        transformationRules
+      },
+      substitution = Reverse[IntegerDigits[i, k, Length[variables]]];
+      transformationRules = MapThread[#1 -> #2 &, {variables, substitution}];
+      template[["rawList"]] /. transformationRules
+    ];
+
+RawExpansion[template_Association] :=
+    With[
+      {
+        substitutionRange = Range[0, (template[["k"]]^Length[RuleTemplateVars[template]])-1]
+      },
+      RawExpansion[template, #] & /@ substitutionRange
+    ];
+
+End[];
+EndPackage[];
