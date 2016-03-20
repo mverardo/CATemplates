4f1fd0f231566d1d83fbf14f7f5e1541302d5562
mverardo <mauricio.verardo@gmail.com>
2016-03-20 19:16:38 -0300

    ModularTemplateIntersection now returns a template built with BuildTemplate[]

--- TemplateOperations/Intersection/ModularTemplateIntersection.wl
+++ TemplateOperations/Intersection/ModularTemplateIntersection.wl
@@ -2,8 +2,9 @@
 
 BeginPackage[
   "CATemplates`TemplateOperations`Intersection`ModularTemplateIntersection`", 
-  { 
-    "CATemplates`Basic`", 
+  {
+    "CATemplates`Basic`",
+    "CATemplates`TemplateGeneration`TemplateFactory`",
     "CATemplates`TemplateOperations`Intersection`Common`"}];
 
 
@@ -13,16 +14,25 @@ ModularTemplateIntersection::usage = "TemplateIntersection[t1_List, t2_List]: Re
 Begin["`Private`"];
 
 
-ModularTemplateIntersection[template1_, template2_, k_] :=
+ModularIntersection[template1_, template2_, k_] :=
   Module[{replacementRules, convertedReplacementRules},
 	replacementRules = ReplacementRules[template1, template2, k];
     If[replacementRules == {}, 
       Return[{}]
     ];
-    (*When a modular template returns 2 different sets of replacement rules, they are both equivalents in terms of ExpandTemplateModK. 
+    (*When a modular template returns 2 different sets of replacement rules, they both have equivalent expansions.
       For that reason, only one of the returned sets is taken into account. *)
     convertedReplacementRules = ConstantsToVariables[First[replacementRules]];
     template1 /. convertedReplacementRules];
+
+ModularTemplateIntersection[template1_Association, template2_Association] :=
+    With[{
+      k = template1[["k"]],
+      r = template1[["r"]],
+      expansion = template1[["expansionFunction"]],
+      rawTemplate1 = template1[["rawList"]],
+      rawTemplate2 = template2[["rawList"]]},
+      BuildTemplate[k, r, ModularIntersection[rawTemplate1, rawTemplate2, k], expansion]];
 
 
 End[];
--- Tests/TemplateOperations/Intersection/ModularTemplateIntersection.m
+++ Tests/TemplateOperations/Intersection/ModularTemplateIntersection.m
@@ -1,28 +1,44 @@
 (* ::Package:: *)
 
-<<CATemplates`
+<<CATemplates`;
 
+report = TestReport[
+  {
+    VerificationTest[
+      With[{
+        t1 = BuildTemplate[3, 1.0, {x1, x0}, RawExpansion]},
+        TemplateIntersection[t1, t1] === t1]],
+    VerificationTest[
+      With[{
+        t1 = BuildTemplate[3, 1.0, {x1, x0}, RawExpansion],
+        t2 = BuildTemplate[3, 1.0, {1, x0}, RawExpansion]},
+        TemplateIntersection[t1, t2] === t2]],
+    VerificationTest[
+      With[{
+        t1 = BuildTemplate[3, 1.0, {x2, 1, x0}, RawExpansion],
+        t2 = BuildTemplate[3, 1.0, {x2, x1, x0}, RawExpansion]},
+        TemplateIntersection[t1, t2] === t1]],
+    VerificationTest[
+      With[{
+        t1 = BuildTemplate[3, 1.0, {x1, x0}, RawExpansion],
+        t2 = BuildTemplate[3, 1.0, {1, x0}, RawExpansion],
+        t3 = BuildTemplate[3, 1.0, {0, x0}, RawExpansion]},
+        With[{i1 = ModularTemplateIntersection[t1, t2],
+          i2 = ModularTemplateIntersection[t1, t3],
+          result = BuildTemplate[3, 1.0, {}, RawExpansion]},
+          ModularTemplateIntersection[i1, i2] === result]]],
+    VerificationTest[
+      With[{
+        t1 = BuildTemplate[3, 1.0, {x1, x0}, RawExpansion],
+        t2 = BuildTemplate[3, 1.0, {1, x0}, RawExpansion],
+        t3 = BuildTemplate[3, 1.0, {x1, 0}, RawExpansion]},
+        With[{
+          i1 = ModularTemplateIntersection[t1, t2],
+          i2 = ModularTemplateIntersection[t1, t3],
+          result = BuildTemplate[3, 1.0, {1, 0}, RawExpansion]},
+          ModularTemplateIntersection[i1, i2] === result]]]
+  }
+];
 
-Print[ModularTemplateIntersection[{x1, x0}, {x1, x0}, 3] === {x1, x0}]
-
-
-Print[ModularTemplateIntersection[{x1, x0}, {1, x0}, 3] === {1, x0}]
-
-
-Print[ModularTemplateIntersection[{x2, x1, x0}, {x2, 1, x0}, 3] === {x2, 1, x0}]
-
-
-Print[
-With[
- {i1 = ModularTemplateIntersection[{x1, x0}, {1, x0}, 3],
-  i2 = ModularTemplateIntersection[{x1, x0}, {0, x0}, 3]},
- ModularTemplateIntersection[i1, i2, 3] === {}]
-]
-
-
-Print[
-With[
- {i1 = ModularTemplateIntersection[{x1, x0}, {1, x0}, 3],
-  i2 = {x1, 0}},
- ModularTemplateIntersection[i1, i2, 3] === {1, 0}]
-]
+Print["Suceeded: " <> ToString[report["TestsSucceededCount"]]];
+Print["Failed: " <> ToString[report["TestsFailedCount"]]];
