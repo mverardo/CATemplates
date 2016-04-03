86548becc70a27fbf300a2e996d37cd77661f07a
mverardo <mauricio.verardo@gmail.com>
2016-04-03 14:16:38 -0300

    Use TestReport on FilteredExpansion

--- Tests/TemplateOperations/Expansion/FilteredExpansion.m
+++ Tests/TemplateOperations/Expansion/FilteredExpansion.m
@@ -5,37 +5,34 @@
 
 << CATemplates`;
 
-With[
-  {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,1}, FilteredExpansion]},
-  Print[FilteredExpansion[T] === {{0,1,0,1,0,1,0,1}}]
-];
+report = TestReport[{
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0, 1, 0, 1, 0, 1, 0, 1}, FilteredExpansion]},
+      FilteredExpansion[T] === {{0, 1, 0, 1, 0, 1, 0, 1}}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0, 1, 0, 1, 0, 1, 0, x0}, FilteredExpansion]},
+      Sort @ FilteredExpansion[T] === Sort @ {{0, 1, 0, 1, 0, 1, 0, 1}, {0, 1, 0, 1, 0, 1, 0, 0}}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0, 1, 0, 1, 0, 1, 0, x0}, FilteredExpansion]},
+      FilteredExpansion[T, 0] === {0, 1, 0, 1, 0, 1, 0, 0}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0, 1, 0, 1, 0, 1, 0, x0}, FilteredExpansion]},
+      FilteredExpansion[T, 1] === {0, 1, 0, 1, 0, 1, 0, 1}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {x7, 1, 0, 1, 0, 1, 0, x0}, FilteredExpansion]},
+      Sort[FilteredExpansion[T]] === Sort[{{0, 1, 0, 1, 0, 1, 0, 1}, {0, 1, 0, 1, 0, 1, 0, 0}, {1, 1, 0, 1, 0, 1, 0, 1}, {1, 1, 0, 1, 0, 1, 0, 0}}]]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0, 1, 0, 1, 0, 1, 0, 1 + x0}, FilteredExpansion]},
+      Sort @ FilteredExpansion[T] === Sort @ {{0, 1, 0, 1, 0, 1, 0, 1}}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {1 + x7, 0, 1, 0, 0, 1, 0, 1 - x0}, FilteredExpansion]},
+      Sort @ FilteredExpansion[T] === Sort @ {{1, 0, 1, 0, 0, 1, 0, 1}, {1, 0, 1, 0, 0, 1, 0, 0}}]]}];
 
-With[
-  {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, FilteredExpansion]},
-  Print[Sort @ FilteredExpansion[T] === Sort @ {{0,1,0,1,0,1,0,1}, {0,1,0,1,0,1,0,0}}]
-];
-
-With[
-  {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, FilteredExpansion]},
-  Print[FilteredExpansion[T, 0] === {0,1,0,1,0,1,0,0}]
-];
-
-With[
-  {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, FilteredExpansion]},
-  Print[FilteredExpansion[T, 1] === {0,1,0,1,0,1,0,1}]
-];
-
-With[
-  {T = BuildTemplate[2, 1.0, {x7,1,0,1,0,1,0,x0}, FilteredExpansion]},
-  Print[Sort[FilteredExpansion[T]] === Sort[{{0,1,0,1,0,1,0,1}, {0,1,0,1,0,1,0,0}, {1,1,0,1,0,1,0,1}, {1,1,0,1,0,1,0,0}}]]
-];
-
-With[
-  {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,1+x0}, FilteredExpansion]},
-  Print[Sort @ FilteredExpansion[T] === Sort @ {{0,1,0,1,0,1,0,1}}]
-];
-
-With[
-  {T = BuildTemplate[2, 1.0, {1 + x7, 0, 1, 0, 0, 1, 0, 1 - x0}, FilteredExpansion]},
-  Print[Sort @ FilteredExpansion[T] === Sort @ {{1, 0, 1, 0, 0, 1, 0, 1}, {1, 0, 1, 0, 0, 1, 0, 0}}]
-];
+PrintTestResults[report];
