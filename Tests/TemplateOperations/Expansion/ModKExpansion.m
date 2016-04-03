432c3126d8ef4fcd4bb561fa0405db02688c4293
mverardo <mauricio.verardo@gmail.com>
2016-04-03 15:22:18 -0300

    Port old ExpandTemplateModK tests to new versions

--- Tests/TemplateOperations/Expansion/ModKExpansion.m
+++ Tests/TemplateOperations/Expansion/ModKExpansion.m
@@ -8,27 +8,49 @@
 report = TestReport[{
   VerificationTest[
     With[
-      {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,1}, ModKExpansion, 2]},
-      ModKExpansion[T] === {{0,1,0,1,0,1,0,1}}]],
+      {T = BuildTemplate[2, 1.0, {}, ModKExpansion, 2]},
+      ModKExpansion[T] === {{}}]],
   VerificationTest[
     With[
-      {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, ModKExpansion, 2]},
-      Sort @ ModKExpansion[T] === Sort @ {{0,1,0,1,0,1,0,1}, {0,1,0,1,0,1,0,0}}]],
+      {T = BuildTemplate[2, 1.0, {2 - x0, x0}, ModKExpansion, 2]},
+      ModKExpansion[T] === {{0, 0}, {1, 1}}]],
   VerificationTest[
     With[
-      {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, ModKExpansion, 2]},
-      ModKExpansion[T, 0] === {0,1,0,1,0,1,0,0}]],
+      {T = BuildTemplate[2, 1.0, {3 - x0, x0}, ModKExpansion, 2]},
+      ModKExpansion[T] === {{1, 0}, {0, 1}}]],
   VerificationTest[
     With[
-      {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, ModKExpansion, 2]},
-      ModKExpansion[T, 1] === {0,1,0,1,0,1,0,1}]],
+      {T = BuildTemplate[3, 1.0, {3 - x0, x0}, ModKExpansion, 2]},
+      ModKExpansion[T] === {{0, 0}, {2, 1}, {1, 2}}]],
   VerificationTest[
     With[
-      {T = BuildTemplate[2, 1.0, {x7,1,0,1,0,1,0,x0}, ModKExpansion, 2]},
-      Sort[ModKExpansion[T]] === Sort[{{0,1,0,1,0,1,0,1}, {0,1,0,1,0,1,0,0}, {1,1,0,1,0,1,0,1}, {1,1,0,1,0,1,0,0}}]]],
+      {T = BuildTemplate[3, 1.0, {1 + x2, x2, 1 + x0, x0}, ModKExpansion, 2]},
+      ModKExpansion[T] === {
+        {1, 0, 1, 0}, {1, 0, 2, 1}, {1, 0, 0, 2}, {2, 1, 1, 0},
+        {2, 1, 2, 1}, {2, 1, 0, 2}, {0, 2, 1, 0}, {0, 2, 2, 1}, {0, 2, 0, 2}}]],
   VerificationTest[
     With[
-      {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,1+x0}, ModKExpansion, 2]},
+      {T = BuildTemplate[2, 1.0, {0, 1, 0, 1, 0, 1, 0, 1}, ModKExpansion, 2]},
+      ModKExpansion[T] === {{0, 1, 0, 1, 0, 1, 0, 1}}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0, 1, 0, 1, 0, 1, 0, x0}, ModKExpansion, 2]},
+      Sort @ ModKExpansion[T] === Sort @ {{0, 1, 0, 1, 0, 1, 0, 1}, {0, 1, 0, 1, 0, 1, 0, 0}}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0, 1, 0, 1, 0, 1, 0, x0}, ModKExpansion, 2]},
+      ModKExpansion[T, 0] === {0, 1, 0, 1, 0, 1, 0, 0}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0, 1, 0, 1, 0, 1, 0, x0}, ModKExpansion, 2]},
+      ModKExpansion[T, 1] === {0, 1, 0, 1, 0, 1, 0, 1}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {x7, 1, 0, 1, 0, 1, 0, x0}, ModKExpansion, 2]},
+      Sort[ModKExpansion[T]] === Sort[{{0, 1, 0, 1, 0, 1, 0, 1}, {0, 1, 0, 1, 0, 1, 0, 0}, {1, 1, 0, 1, 0, 1, 0, 1}, {1, 1, 0, 1, 0, 1, 0, 0}}]]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0, 1, 0, 1, 0, 1, 0, 1 + x0}, ModKExpansion, 2]},
       Sort @ ModKExpansion[T] === Sort @ {{0, 1, 0, 1, 0, 1, 0, 1}, {0, 1, 0, 1, 0, 1, 0, 0}}]],
   VerificationTest[
     With[
