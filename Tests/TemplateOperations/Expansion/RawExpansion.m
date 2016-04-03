c5a95e766dfaf7dcf835a17211cfcd6913b117d5
mverardo <mauricio.verardo@gmail.com>
2016-04-03 14:23:59 -0300

    Use TestReport on RawExpansion

--- Tests/TemplateOperations/Expansion/RawExpansion.m
+++ Tests/TemplateOperations/Expansion/RawExpansion.m
@@ -5,32 +5,30 @@
 
 << CATemplates`;
 
-With[
-  {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,1}, RawExpansion]},
-  Print[RawExpansion[T] === {{0,1,0,1,0,1,0,1}}]
-];
+report = TestReport[{
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,1}, RawExpansion]},
+      RawExpansion[T] === {{0,1,0,1,0,1,0,1}}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, RawExpansion]},
+      Sort @ RawExpansion[T] === Sort @ {{0,1,0,1,0,1,0,1}, {0,1,0,1,0,1,0,0}}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, RawExpansion]},
+      RawExpansion[T, 0] === {0,1,0,1,0,1,0,0}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, RawExpansion]},
+      RawExpansion[T, 1] === {0,1,0,1,0,1,0,1}]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {x7,1,0,1,0,1,0,x0}, RawExpansion]},
+      Sort[RawExpansion[T]] === Sort[{{0,1,0,1,0,1,0,1}, {0,1,0,1,0,1,0,0}, {1,1,0,1,0,1,0,1}, {1,1,0,1,0,1,0,0}}]]],
+  VerificationTest[
+    With[
+      {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,1+x0}, RawExpansion]},
+      Sort @ RawExpansion[T] === Sort @ {{0,1,0,1,0,1,0,2}, {0,1,0,1,0,1,0,1}}]]}];
 
-With[
-  {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, RawExpansion]},
-  Print[Sort @ RawExpansion[T] === Sort @ {{0,1,0,1,0,1,0,1}, {0,1,0,1,0,1,0,0}}]
-];
-
-With[
-  {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, RawExpansion]},
-  Print[RawExpansion[T, 0] === {0,1,0,1,0,1,0,0}]
-];
-
-With[
-  {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,x0}, RawExpansion]},
-  Print[RawExpansion[T, 1] === {0,1,0,1,0,1,0,1}]
-];
-
-With[
-  {T = BuildTemplate[2, 1.0, {x7,1,0,1,0,1,0,x0}, RawExpansion]},
-  Print[Sort[RawExpansion[T]] === Sort[{{0,1,0,1,0,1,0,1}, {0,1,0,1,0,1,0,0}, {1,1,0,1,0,1,0,1}, {1,1,0,1,0,1,0,0}}]]
-];
-
-With[
-  {T = BuildTemplate[2, 1.0, {0,1,0,1,0,1,0,1+x0}, RawExpansion]},
-  Print[Sort @ RawExpansion[T] === Sort @ {{0,1,0,1,0,1,0,2}, {0,1,0,1,0,1,0,1}}]
-];
+PrintTestResults[report];
