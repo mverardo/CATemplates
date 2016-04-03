c0625f32ca305cd11aae078c425fdaecdded9a38
mverardo <mauricio.verardo@gmail.com>
2016-04-03 13:51:19 -0300

    Adapt ModNConservingTemplate to use BuildTemplate
    Also removes duplicated code and refactors tests to use TestReports.

--- Tests/TemplateGeneration/StateConservingTemplate.m
+++ Tests/TemplateGeneration/StateConservingTemplate.m
@@ -2,16 +2,21 @@
 
 << CATemplates`;
 
-
-report = TestReport[
-  {
-    VerificationTest[
-      StateConservingTemplate[] === BuildTemplate[2, 1.0, {1, 1 + x2 - x3, 1 - x2, 1 - x1 - x2, x3, x2, x1, 0}, FilteredExpansion]],
-    VerificationTest[
-      StateConservingTemplate[3, 1.0] === BuildTemplate[3, 1.0, {2, 2 + x7 - x8, 2 + x6 - x8, 2 + x1 - x2 + x5 - x7, 2 + x1 - x2 + x4 - x7, 2 + x1 - x2 + x3 - x7, 2 - x6, 2 + x1 - x2 - x6, 2 - x2 - x6, 1 - x1 + x2 - x5 + x8, 1 - x1 + x2 - x5 + x7, 1 - x1 + x2 - x5 + x6, 1 - x4 + x5, 1, 1 + x3 - x4, 1 - x1 + x2 - x3, 1 - x3, 1 - x1 - x3, x8, x7, x6, x5, x4, x3, x2, x1, 0}, FilteredExpansion]],
-    VerificationTest[
-      StateConservingTemplate[2, 2.0] === BuildTemplate[2, 2.0, {1, 1 + x14 - x15, 1 + x13 - x14 + x6 - x7, 1 + x12 - x14 + x6 - x7, 1 + x11 - x13 + x2 - x3 + x5 - x6, 1 + x10 - x13 + x2 - x3 + x5 - x6, 1 - x12 + x2 - x3 + x4 - x6 + x9, 1 - x12 + x2 - x3 + x4 - x6 + x8, 1 - x11 - x2 + x3 - x5 + x7, 1 - x11 - x2 + x3 - x5 + x6, 1 - x10, 1 - x10 + x4 - x5, 1 - x2 + x3 - x4 - x9, 1 - x4 - x9, 1 - x2 - x4 - x8, 1 - x1 - x2 - x4 - x8, x15, x14, x13, x12, x11, x10, x9, x8, x7, x6, x5, x4, x3, x2, x1, 0}, FilteredExpansion]],
-    VerificationTest[
-      ExpandTemplate[StateConservingTemplate[]] == {{1, 1, 1, 1, 0, 0, 0, 0}, {1, 1, 1, 0, 0, 0, 1, 0}, {1, 0, 1, 1, 1, 0, 0, 0}, {1, 0, 1, 0, 1, 0, 1, 0}, {1, 1, 0, 0, 1, 1, 0, 0}}]}];
+report = TestReport[{
+  VerificationTest[
+    StateConservingTemplate[] === BuildTemplate[2, 1.0, {1,1+x2-x3,1-x2,1-x1-x2,x3,x2,x1,0}, FilteredExpansion]],
+  VerificationTest[
+    StateConservingTemplate[3,1.0] === BuildTemplate[3, 1.0, {2,2+x7-x8,2+x6-x8,2+x1-x2+x5-x7,2+x1-x2+x4-x7,2+x1-x2+x3-x7,2-x6,2+x1-x2-x6,2-x2-x6,1-x1+x2-x5+x8,1-x1+x2-x5+x7,1-x1+x2-x5+x6,1-x4+x5,1,1+x3-x4,1-x1+x2-x3,1-x3,1-x1-x3,x8,x7,x6,x5,x4,x3,x2,x1,0}, FilteredExpansion]],
+  VerificationTest[
+    StateConservingTemplate[2,2.0] === BuildTemplate[2, 2.0, {1,1+x14-x15,1+x13-x14+x6-x7,1+x12-x14+x6-x7,1+x11-x13+x2-x3+x5-x6,1+x10-x13+x2-x3+x5-x6,1-x12+x2-x3+x4-x6+x9,1-x12+x2-x3+x4-x6+x8,1-x11-x2+x3-x5+x7,1-x11-x2+x3-x5+x6,1-x10,1-x10+x4-x5,1-x2+x3-x4-x9,1-x4-x9,1-x2-x4-x8,1-x1-x2-x4-x8,x15,x14,x13,x12,x11,x10,x9,x8,x7,x6,x5,x4,x3,x2,x1,0}, FilteredExpansion]],
+  VerificationTest[
+    ExpandTemplate[StateConservingTemplate[]]=={{1,1,1,1,0,0,0,0},{1,1,1,0,0,0,1,0},{1,0,1,1,1,0,0,0},{1,0,1,0,1,0,1,0},{1,1,0,0,1,1,0,0}}],
+  VerificationTest[
+    Sort[FromDigits[#, 2] & /@ ExpandTemplate[ModNStateConservingTemplate[2]]] == {132, 150, 170, 184, 204, 222, 226, 240}],
+  VerificationTest[
+    PreservesIndexVariableDualityQ[ModNStateConservingTemplate[2][["rawList"]]]],
+  VerificationTest[
+    PreservesIndexVariableDualityQ[ModNStateConservingTemplate[3][["rawList"]]]]
+}];
 
 PrintTestResults[report];
