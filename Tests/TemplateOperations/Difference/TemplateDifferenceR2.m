43604e7543bd74503c6f1422c66fde8683a683ca
Zorandir Soares <zorandir@gmail.com>
2016-08-15 03:08:25 -0300

    Commented test in large spaces

--- Tests/TemplateOperations/Difference/TemplateDifferenceR2.m
+++ Tests/TemplateOperations/Difference/TemplateDifferenceR2.m
@@ -1,142 +1,142 @@
 (* ::Package:: *)
 
-<< CATemplates`;
+(*<< CATemplates`;*)
 
-colorBT2 = ColorBlindTemplate[2, 2.0];
-colorBR2 = ExpandTemplate[colorBT2];
-totalT2 = TotalisticTemplate[2, 2.0];
-totalR2 = ExpandTemplate[totalT2];
-outTotT2 = OuterTotalisticTemplate[2, 2.0];
-outTotR2 = ExpandTemplate[outTotT2];
+(*colorBT2 = ColorBlindTemplate[2, 2.0];*)
+(*colorBR2 = ExpandTemplate[colorBT2];*)
+(*totalT2 = TotalisticTemplate[2, 2.0];*)
+(*totalR2 = ExpandTemplate[totalT2];*)
+(*outTotT2 = OuterTotalisticTemplate[2, 2.0];*)
+(*outTotR2 = ExpandTemplate[outTotT2];*)
 
-Print["ColorBlindTemplate DiffTest2"];
-colorBTDiffTest2 = TestReport[
-  {
-    VerificationTest[
-      With[{
-        t1 = colorBT2,
-        t2 = colorBT2
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expectedr = {};
-        ContainsExactly[fra,expectedr]]],
-    VerificationTest[
-      With[{
-        t1 = colorBT2,
-        t2 = totalT2,
-        r1 = colorBR2,
-        r2 = totalR2
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]],
-    VerificationTest[
-      With[{
-        t1 = colorBT2,
-        t2 = outTotT2,
-        r1 = colorBR2,
-        r2 = outTotR2
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]]
-  }];
-PrintTestResults[colorBTDiffTest2];
+(*Print["ColorBlindTemplate DiffTest2"];*)
+(*colorBTDiffTest2 = TestReport[*)
+  (*{*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = colorBT2,*)
+        (*t2 = colorBT2*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expectedr = {};*)
+        (*ContainsExactly[fra,expectedr]]],*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = colorBT2,*)
+        (*t2 = totalT2,*)
+        (*r1 = colorBR2,*)
+        (*r2 = totalR2*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]],*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = colorBT2,*)
+        (*t2 = outTotT2,*)
+        (*r1 = colorBR2,*)
+        (*r2 = outTotR2*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]]*)
+  (*}];*)
+(*PrintTestResults[colorBTDiffTest2];*)
 
-Print["TotalisticTemplate DiffTest2"];
-stateCTDiffTest2 = TestReport[
-  {
-    VerificationTest[
-      With[{
-        t1 = totalT2,
-        t2 = colorBT2,
-        r1 = totalR2,
-        r2 = colorBR2
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]],
-    VerificationTest[
-      With[{
-        t1 = totalT2,
-        t2 = totalT2,
-        r1 = totalR2,
-        r2 = totalR2
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]],
-    VerificationTest[
-      With[{
-        t1 = totalT2,
-        t2 = outTotT2,
-        r1 = totalR2,
-        r2 = outTotR2
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]]
-  }];
-PrintTestResults[stateCTDiffTest2];
+(*Print["TotalisticTemplate DiffTest2"];*)
+(*stateCTDiffTest2 = TestReport[*)
+  (*{*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = totalT2,*)
+        (*t2 = colorBT2,*)
+        (*r1 = totalR2,*)
+        (*r2 = colorBR2*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]],*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = totalT2,*)
+        (*t2 = totalT2,*)
+        (*r1 = totalR2,*)
+        (*r2 = totalR2*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]],*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = totalT2,*)
+        (*t2 = outTotT2,*)
+        (*r1 = totalR2,*)
+        (*r2 = outTotR2*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]]*)
+  (*}];*)
+(*PrintTestResults[stateCTDiffTest2];*)
 
-Print["OuterTotalisticTemplate DiffTest2"];
-stateCTDiffTest2 = TestReport[
-  {
-    VerificationTest[
-      With[{
-        t1 = outTotT2,
-        t2 = colorBT2,
-        r1 = outTotR2,
-        r2 = colorBR2
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]],
-    VerificationTest[
-      With[{
-        t1 = outTotT2,
-        t2 = totalT2,
-        r1 = outTotR2,
-        r2 = totalR2
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]],
-    VerificationTest[
-      With[{
-        t1 = outTotT2,
-        t2 = outTotT2,
-        r1 = outTotR2,
-        r2 = outTotR2
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]]
-  }];
-PrintTestResults[stateCTDiffTest2];
+(*Print["OuterTotalisticTemplate DiffTest2"];*)
+(*stateCTDiffTest2 = TestReport[*)
+  (*{*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = outTotT2,*)
+        (*t2 = colorBT2,*)
+        (*r1 = outTotR2,*)
+        (*r2 = colorBR2*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]],*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = outTotT2,*)
+        (*t2 = totalT2,*)
+        (*r1 = outTotR2,*)
+        (*r2 = totalR2*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]],*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = outTotT2,*)
+        (*t2 = outTotT2,*)
+        (*r1 = outTotR2,*)
+        (*r2 = outTotR2*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]]*)
+  (*}];*)
+(*PrintTestResults[stateCTDiffTest2];*)
--- Tests/TemplateOperations/Difference/TemplateDifferenceR3.m
+++ Tests/TemplateOperations/Difference/TemplateDifferenceR3.m
@@ -1,72 +1,72 @@
 (* ::Package:: *)
 
-<< CATemplates`;
+(*<< CATemplates`;*)
 
-totalT3 = TotalisticTemplate[2, 3.0];
-outTotT3 = OuterTotalisticTemplate[2, 3.0];
-totalR3  = ExpandTemplate[totalT3];
-outTotR3 = ExpandTemplate[outTotT3];
+(*totalT3 = TotalisticTemplate[2, 3.0];*)
+(*outTotT3 = OuterTotalisticTemplate[2, 3.0];*)
+(*totalR3  = ExpandTemplate[totalT3];*)
+(*outTotR3 = ExpandTemplate[outTotT3];*)
 
-Print["TotalisticTemplate DiffTest3"];
-stateCTDiffTest3 = TestReport[
-  {
-    VerificationTest[
-      With[{
-        t1 = totalT3,
-        t2 = totalT3,
-        r1 = totalR3,
-        r2 = totalR3
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]],
-    VerificationTest[
-      With[{
-        t1 = totalT3,
-        t2 = outTotT3,
-        r1 = totalR3,
-        r2 = outTotR3
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]]
-  }];
-PrintTestResults[stateCTDiffTest3];
+(*Print["TotalisticTemplate DiffTest3"];*)
+(*stateCTDiffTest3 = TestReport[*)
+  (*{*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = totalT3,*)
+        (*t2 = totalT3,*)
+        (*r1 = totalR3,*)
+        (*r2 = totalR3*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]],*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = totalT3,*)
+        (*t2 = outTotT3,*)
+        (*r1 = totalR3,*)
+        (*r2 = outTotR3*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]]*)
+  (*}];*)
+(*PrintTestResults[stateCTDiffTest3];*)
 
-Print["OuterTotalisticTemplate DiffTest2"];
-stateCTDiffTest2 = TestReport[
-  {
-    VerificationTest[
-      With[{
-        t1 = outTotT3,
-        t2 = totalT3,
-        r1 = outTotR3,
-        r2 = totalR3
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]],
-    VerificationTest[
-      With[{
-        t1 = outTotT3,
-        t2 = outTotT3,
-        r1 = outTotR3,
-        r2 = outTotR3
-      },
-        diffa = TemplateDifferenceDirect[t1, t2];
-        ra = ExpandTemplate /@ diffa;
-        fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];
-        expected = Complement[r1,r2];
-        expectedr = Sort[FromDigits[#, 2] & /@ expected];
-        ContainsExactly[fra,expectedr]]]
-  }];
-PrintTestResults[stateCTDiffTest2];
+(*Print["OuterTotalisticTemplate DiffTest2"];*)
+(*stateCTDiffTest2 = TestReport[*)
+  (*{*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = outTotT3,*)
+        (*t2 = totalT3,*)
+        (*r1 = outTotR3,*)
+        (*r2 = totalR3*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]],*)
+    (*VerificationTest[*)
+      (*With[{*)
+        (*t1 = outTotT3,*)
+        (*t2 = outTotT3,*)
+        (*r1 = outTotR3,*)
+        (*r2 = outTotR3*)
+      (*},*)
+        (*diffa = TemplateDifferenceDirect[t1, t2];*)
+        (*ra = ExpandTemplate /@ diffa;*)
+        (*fra = Sort[FromDigits[#, 2] & /@DeleteDuplicates[Join@@ra]];*)
+        (*expected = Complement[r1,r2];*)
+        (*expectedr = Sort[FromDigits[#, 2] & /@ expected];*)
+        (*ContainsExactly[fra,expectedr]]]*)
+  (*}];*)
+(*PrintTestResults[stateCTDiffTest2];*)
